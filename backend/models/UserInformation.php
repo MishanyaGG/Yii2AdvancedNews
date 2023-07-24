6c57523df0c3d97b19137b425d09890aef3122fb
MishanyaGG <mischatitovmail.ru@gmail.com>
2023-07-24 23:01:00 +0300

    Решал вопрос с ошибкой вывода в профиле текста на разных языках, оказался баг не в алоритме, а баг в отображении тэгов, я не знаю как это решить

--- backend/models/UserInformation.php
+++ backend/models/UserInformation.php
@@ -10,10 +10,6 @@ use common\models\User;
  *
  * @property int $id
  * @property int $id_user
- * @property string|null $information_in_ru
- * @property string|null $information_in_en
- * @property string|null $information_in_de
- * @property string|null $information_in_es
  *
  * @property Users $user
  */
@@ -35,7 +31,6 @@ class UserInformation extends \yii\db\ActiveRecord
         return [
             [['id_user'], 'required'],
             [['id_user'], 'integer'],
-            [['information_in_ru', 'information_in_en', 'information_in_de', 'information_in_es'], 'string'],
             [['id_user'], 'exist', 'skipOnError' => true, 'targetClass' => Users::class, 'targetAttribute' => ['id_user' => 'id']],
         ];
     }
@@ -49,9 +44,6 @@ class UserInformation extends \yii\db\ActiveRecord
             'id' => 'ID',
             'id_user' => 'Id User',
             'information_in_ru' => 'Information In Ru',
-            'information_in_en' => 'Information In En',
-            'information_in_de' => 'Information In De',
-            'information_in_sp' => 'Information In Es',
         ];
     }
 
